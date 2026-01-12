0115ce109c9b5d3143e926f0078f404ac7345129
Abdallah Elkilany <asdghalb@gmail.com>
2026-01-12 15:41:25 +0200

    Initial Expo + TypeScript setup with project structure

---
+++ src/screens/TaskListScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View,Text } from "react-native"
+
+const TaskListScreen = () => {
+    return (
+        <View>
+            <Text>Task List</Text>
+        </View>
+    )
+}
+
+export default TaskListScreen
